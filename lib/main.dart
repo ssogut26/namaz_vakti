d3955ef6b90a175078f8961862b167ded4359a80
ssogut26 <ssogut26@gmail.com>
2023-07-14 18:04:36 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:namaz_vakti/app.dart';
-import 'package:namaz_vakti/generated/locale_keys.g.dart';
 import 'package:namaz_vakti/models/prayer_times.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,7 +21,7 @@ Future<void> main() async {
           Locale('en', 'US'),
           Locale('tr', 'TR'),
         ],
-        assetLoader: const CodegenLoader(),
+
         fallbackLocale: const Locale('en', 'US'),
         path: 'assets/lang', // <-- change the path of the translation files
         child: NamazVaktiApp(prefs: prefs),
